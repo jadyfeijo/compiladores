5872089e32a122f3ed4ad491d9a045b6d0e367ac
Matheus Luz <msluz@inf.ufrgs.br>
2016-05-30 17:28:52 -0300

    adding generate code

---
+++ Etapa 5/genco.c
@@ -0,0 +1,9 @@
+//
+//  genco.c
+//  
+//
+//  Created by Matheus Stefanello Luz on 5/30/16.
+//
+//
+
+#include "genco.h"
---
+++ Etapa 5/genco.h
@@ -0,0 +1,18 @@
+//
+//  genco.h
+//  
+//
+//  Created by Matheus Stefanello Luz on 5/30/16.
+//
+//
+
+#ifndef genco_h
+#define genco_h
+
+#include <stdio.h>
+#include "astree.h"
+#include "tacs.h"
+
+TAC* generateCode(AST* node);
+
+#endif /* genco_h */
